2790ffe90777c318e078ce7b68cb51a1eebd7e86
TANKEU <takeuyopaariane@gmail.com>
2025-04-07 16:34:28 +0100

    modify web config

--- src/main/java/com/myapp/localizationApp/configuration/WebConfig.java
+++ src/main/java/com/myapp/localizationApp/configuration/WebConfig.java
@@ -12,11 +12,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders(HttpHeaders.CONTENT_DISPOSITION)
                         .allowCredentials(true);
-
     }
 }
